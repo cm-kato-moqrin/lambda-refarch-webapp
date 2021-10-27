955804b8eff8a6669a5a6cc2d370de5f18945cb1
cmoqrin <kato.teiji@classmethod.jp>
2021-10-27 18:42:00 +0900

    update App.js

--- www/src/App.js
+++ www/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Jumbotron, Row, Col, Alert, Button } from 'reactstrap';
 import axios from 'axios';
-import ToDo from './ToDo'
+import ToDo from './ToDo';
 
 import './App.css';
 import logo from './aws.png';
@@ -23,13 +23,16 @@ function App() {
     }
   }, [idToken]);
 
-  axios.interceptors.response.use(response => {
-    console.log('Response was received');
-    return response;
-  }, error => {
-    window.location.href = config.redirect_url;
-    return Promise.reject(error);
-  });
+  axios.interceptors.response.use(
+    (response) => {
+      console.log('Response was received');
+      return response;
+    },
+    (error) => {
+      window.location.href = config.redirect_url;
+      return Promise.reject(error);
+    }
+  );
 
   function onDismiss() {
     setAlertVisible(false);
@@ -44,14 +47,14 @@ function App() {
 
   const clearCredentials = () => {
     window.location.href = config.redirect_url;
-  }
+  };
 
   const getIdToken = () => {
     const hash = window.location.hash.substr(1);
-    const objects = hash.split("&");
-    objects.forEach(object => {
-      const keyVal = object.split("=");
-      if (keyVal[0] === "id_token") {
+    const objects = hash.split('&');
+    objects.forEach((object) => {
+      const keyVal = object.split('=');
+      if (keyVal[0] === 'id_token') {
         setIdToken(keyVal[1]);
       }
     });
@@ -61,9 +64,9 @@ function App() {
     const result = await axios({
       url: `${config.api_base_url}/item/`,
       headers: {
-        Authorization: idToken
-      }
-    }).catch(error => {
+        Authorization: idToken,
+      },
+    }).catch((error) => {
       console.log(error);
     });
 
@@ -84,17 +87,17 @@ function App() {
     if (!item || item === '') return;
 
     const newToDo = {
-      "item": item,
-      "completed": false
+      item: item,
+      completed: false,
     };
 
     const result = await axios({
       method: 'POST',
       url: `${config.api_base_url}/item/`,
       headers: {
-        Authorization: idToken
+        Authorization: idToken,
       },
-      data: newToDo
+      data: newToDo,
     });
 
     if (result && result.status === 401) {
@@ -103,20 +106,21 @@ function App() {
       getAllTodos();
       newToDoInput.value = '';
     }
-  }
+  };
 
   const deleteToDo = async (indexToRemove, itemId) => {
     if (indexToRemove === null) return;
     if (itemId === null) return;
 
-    console.log("thought")
+    console.log('thought');
+    console.log(itemId);
 
     const result = await axios({
       method: 'DELETE',
       url: `${config.api_base_url}/item/${itemId}`,
       headers: {
-        Authorization: idToken
-      }
+        Authorization: idToken,
+      },
     });
 
     if (result && result.status === 401) {
@@ -125,7 +129,7 @@ function App() {
       const newToDos = toDos.filter((item, index) => index !== indexToRemove);
       setToDos(newToDos);
     }
-  }
+  };
 
   const completeToDo = async (itemId) => {
     if (itemId === null) return;
@@ -134,49 +138,61 @@ function App() {
       method: 'POST',
       url: `${config.api_base_url}/item/${itemId}/done`,
       headers: {
-        Authorization: idToken
-      }
+        Authorization: idToken,
+      },
     });
 
     if (result && result.status === 200) {
       getAllTodos();
     }
-  }
+  };
 
   return (
-    <div className="App">
+    <div className='App'>
       <Container>
-        <Alert color={alertStyle} isOpen={alertVisible} toggle={alertDismissable ? onDismiss : null}>
+        <Alert
+          color={alertStyle}
+          isOpen={alertVisible}
+          toggle={alertDismissable ? onDismiss : null}
+        >
           <p dangerouslySetInnerHTML={{ __html: alert }}></p>
         </Alert>
         <Jumbotron>
           <Row>
-            <Col md="6" className="logo">
+            <Col md='6' className='logo'>
               <h1>Serverless Todo</h1>
               <p>This is a demo that showcases AWS serverless.</p>
-              <p>The application is built using the SAM CLI toolchain, and uses AWS Lambda, Amazon DynamoDB, and Amazon API Gateway for API services and Amazon Cognito for identity.</p>
+              <p>
+                The application is built using the SAM CLI toolchain, and uses
+                AWS Lambda, Amazon DynamoDB, and Amazon API Gateway for API
+                services and Amazon Cognito for identity.
+              </p>
 
-              <img src={logo} alt="Logo" />
+              <img src={logo} alt='Logo' />
             </Col>
-            <Col md="6">
-              {idToken.length > 0 ?
-                (
-                  <ToDo updateAlert={updateAlert} toDos={toDos} addToDo={addToDo} deleteToDo={deleteToDo} completeToDo={completeToDo} />
-                ) : (
-                  <Button
-                    href={`https://${config.cognito_hosted_domain}/login?response_type=token&client_id=${config.aws_user_pools_web_client_id}&redirect_uri=${config.redirect_url}`}
-                    color="primary"
-                    className="mt-5 float-center"
-                  >
-                    Log In
-                  </Button>
-                )
-              }
+            <Col md='6'>
+              {idToken.length > 0 ? (
+                <ToDo
+                  updateAlert={updateAlert}
+                  toDos={toDos}
+                  addToDo={addToDo}
+                  deleteToDo={deleteToDo}
+                  completeToDo={completeToDo}
+                />
+              ) : (
+                <Button
+                  href={`https://${config.cognito_hosted_domain}/login?response_type=token&client_id=${config.aws_user_pools_web_client_id}&redirect_uri=${config.redirect_url}`}
+                  color='primary'
+                  className='mt-5 float-center'
+                >
+                  Log In
+                </Button>
+              )}
             </Col>
           </Row>
         </Jumbotron>
       </Container>
-    </div >
+    </div>
   );
 }
 
