7c02b10a739068d6f85e2d83cd94f85751f3336c
cm-kato-moqrin <kato.teiji@classmethod.jp>
2022-02-22 17:48:39 +0900

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "",     // CognitoClientID
-  "api_base_url": "",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "",                   // CognitoDomainName
-  "redirect_url": ""                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "52g5bik8si1l621e6gur13cuj2",     // CognitoClientID
+  "api_base_url": "https://guenliande.execute-api.ap-northeast-1.amazonaws.com/prod",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoapporigin-refarch-origin.auth.ap-northeast-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d19zh2enfjhtlm.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
